89af5e98e55c219d2c0aefde9b9ca5a0e21436b6
kseniazilak <kzhilak@bk.ru>
2026-02-08 20:50:23 +0300

    Техническое задание Wordle

--- src/ru/yandex/practicum/exeption/WordNotFoundInDictionary.java
+++ src/ru/yandex/practicum/exeption/WordNotFoundInDictionary.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.exeption;
 
-public class WordNotFoundInDictionary extends Exception{
+public class WordNotFoundInDictionary extends Exception {
 
     public WordNotFoundInDictionary(final String message) {
         super(message);
